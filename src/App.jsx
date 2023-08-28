5ba239de26e33706298d48a91ef919c410bce631
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2023-08-29 00:32:16 +0200

    Added ColorPicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-
-import './App.css'
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
 
 function App() {
- 
+  return <main className="app transition-all ease-in">
+    <Home />
+    <Canvas />
+    <Customizer />
 
-  return
+  </main>;
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import CustomButton from "../components/CustomButton";
+import state from "../store";
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" /> DO IT.
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Create your unique and amazing shirt with our brand new 3D
+                customization tool. <br />
+                <br />
+                <bold>
+                  Unleash your imagination and define your own style,
+                </bold>{" "}
+                <i>
+                  Also we have AI to make you design a{" "}
+                  <strong>SPECTACULAR </strong> brand.
+                  <br />
+                  <strong>Try it out now!</strong> 🚀
+                </i>
+              </p>
+
+              <CustomButton
+                type="filled"
+                title="Customize it"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
